3342da46a3b1f1290a73eb4c1eebf71c190ec94b
MattOz <m.ozuna22@gmail.com>
2024-06-04 16:07:55 -0400

    added insertion sort

--- insertion.js
+++ insertion.js
@@ -1,3 +1,21 @@
-function insertionSort() {}
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[i - 1]) {
+      let temp = arr[i];
+
+      for (let j = i; j > 0; j--) {
+        if (temp >= arr[j - 1]) {
+          arr[j] = temp;
+          break;
+        } else {
+          arr[j] = arr[j - 1];
+          arr[j-1] = temp;
+        }
+      }
+    }
+  }
+
+  return arr;
+}
 
 module.exports = insertionSort;
